ae44cd0a15cc4ee792ba873c948f7ed6beea362b
vaishnaray1 <vaishnavi.narayanan@chicagobooth.edu>
2022-04-18 00:10:18 +0000

    wip

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -32,4 +32,8 @@ class Photo < ApplicationRecord
   validates :caption, presence: true
   validates :image, presence: true
 
+  #scope
+  scope :past_week, ->{where(created_at: 1.week.ago...)}
+  scope :by_likes, -> {order(likes_count: :desc)}
+
 end
